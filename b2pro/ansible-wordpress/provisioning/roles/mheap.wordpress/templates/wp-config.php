e4d5ef98784a8068126e20caa932cb1a838e727b
Prashaath R <arashpath@gmail.com>
2019-03-10 12:36:35 +0530

    Added variables

--- b2pro/ansible-wordpress/provisioning/roles/mheap.wordpress/templates/wp-config.php
+++ b2pro/ansible-wordpress/provisioning/roles/mheap.wordpress/templates/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', '{{database_name}}');
 
 /** MySQL database username */
-define('DB_USER', 'wordpress');
+define('DB_USER', '{{database_user}}');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'bananas');
+define('DB_PASSWORD', '{{database_password}}');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
